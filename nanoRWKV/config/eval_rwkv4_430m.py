3b78ad1dc930a65e24e9851c1f9c598e4086ee72
Sheng-Hao Ma <shenghao_ma@txone.com>
2024-02-15 17:44:01 +0800

    balabala

--- nanoRWKV/config/eval_rwkv4_430m.py
+++
@@ -1,6 +0,0 @@
-batch_size = 8
-eval_iters = 500 # use more iterations to get good estimate
-eval_only = True
-wandb_log = False
-init_from = 'RWKV/rwkv-4-430m-pile'
-dtype = 'float16' # v100 doesn't support bf16
